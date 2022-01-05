d9d718615bf50d606832489035af6a04d6d4cc2e
Dmitry <dmitry.lyovochkin@gmail.com>
2022-01-05 23:20:08 +0300

    работа над category

--- lib/application/ui/screens/home_screen/category_widget.dart
+++ lib/application/ui/screens/home_screen/category_widget.dart
@@ -1,5 +1,12 @@
-import 'package:flutter/foundation.dart';
+import 'package:ecommerce_project/application/ui/screens/home_screen/list_category.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+getSvgIcon(icon) {
+  return SvgPicture.asset(iconPath + icon);
+}
+
+const String iconPath = "assets/icons/";
 
 class CategoryWidget extends StatelessWidget {
   const CategoryWidget({Key? key}) : super(key: key);
@@ -7,103 +14,134 @@ class CategoryWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
+        child: SizedBox(
+      width: double.infinity,
+      child: SingleChildScrollView(
+          padding: const EdgeInsets.only(top: 20),
+          child: Column(children: const [
+            SizedBox(height: 5),
+            _GeolocWidget(),
+            SizedBox(height: 10),
+            _SearchWidget(),
+            SizedBox(height: 5),
+            _HotSalesWidget(),
+            SizedBox(height: 10),
+            SelectedCategory()
+          ])),
+    ));
+  }
+}
+
+class SelectedCategory extends StatefulWidget {
+  const SelectedCategory({Key? key}) : super(key: key);
+
+  @override
+  State<SelectedCategory> createState() => _SelectedCategoryState();
+}
+
+class _SelectedCategoryState extends State<SelectedCategory> {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
       child: Container(
         width: double.infinity,
         child: SingleChildScrollView(
-          padding: const EdgeInsets.only(top: 20),
-          child: Column(
-            children: const [
-              SizedBox(height: 5),
-              _GeolocWidget(),
-              SelectCategoryWidget(),
-              SizedBox(height: 10),
-              _SearchWidget(),
-              SizedBox(height: 5),
-              _HotSalesWidget(),
-              SizedBox(height: 10),
-              // _SectionButtonsWidget(),
-            ]
-          )
+          scrollDirection: Axis.horizontal,
+          child: Row(
+            children: List.generate(listCategory.length, (index) {
+              return Container(
+                width: 120,
+                height: 120,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [getSvgIcon(listCategory[index]['IconPath'])],
+                ),
+              );
+            
+            }),
         ),
       ),
-    );
+    ));
   }
 }
 
-class SelectCategoryWidget extends StatefulWidget {
-  const SelectCategoryWidget({Key? key}) : super(key: key);
+// class SelectCategoryWidget extends StatefulWidget {
+//   const SelectCategoryWidget({Key? key}) : super(key: key);
 
-  @override
-  State<SelectCategoryWidget> createState() => SelectCategoryWidgetState();
-}
+//   @override
+//   State<SelectCategoryWidget> createState() => SelectCategoryWidgetState();
+// }
 
-class SelectCategoryWidgetState extends State<SelectCategoryWidget> {
-  List<Map> categories = [
-    {'name': 'Phone', 'IconPath': 'assets/icons/Phone.png'},
-    {'name': 'Computer', 'IconPath': 'assets/icons/Computer.png'},
-    {'name': 'Health', 'IconPath': 'assets/icons/Health.png'},
-    {'name': 'Books', 'IconPath': 'assets/icons/Books.png'},
-    {'name': 'Phone', 'IconPath': 'assets/icons/Phone.png'},
-    {'name': 'Phone', 'IconPath': 'assets/icons/Phone.png'},
-    {'name': 'Phone', 'IconPath': 'assets/icons/Phone.png'}
-  ];
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.black,
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            const SizedBox(
-              height: 250,
-            ),
-            const Text(
-              'Categories',
-              style: TextStyle(
-                fontSize: 40,
-                fontWeight: FontWeight.w700,
-                color: Colors.amber,
-              ),
-              textAlign: TextAlign.center,
-            ),
-            const SizedBox(height: 40),
-            SizedBox(
-              height: 150,
-              child: ListView.builder(
-                  scrollDirection: Axis.horizontal,
-                  itemCount: categories.length,
-                  itemBuilder: (context, index) {
-                    return Column(
-                      children: [
-                        Container(
-                          padding: const EdgeInsets.all(10),
-                          margin: const EdgeInsets.only(left: 20),
-                          decoration: BoxDecoration(
-                            color: Colors.red,
-                            borderRadius: BorderRadius.circular(20),
-                          ),
-                          child: Image.asset(
-                            categories[index]['IconPath'],
-                            height: 50,
-                          ),
-                        ),
-                        const SizedBox(height: 10),
-                        Text(categories[index]['name'],
-                        style: const TextStyle(
-                          color: Colors.white,
-                        ),
-                          textAlign: TextAlign.center
-                        )
-                      ],
-                    );
-                  }),
-            )
-          ],
-        ),
-      ),
-    );
-  }
-}
+// class SelectCategoryWidgetState extends State<SelectCategoryWidget> {
+//   List<Map> categories = [
+//     {'name': 'Phone', 'IconPath': 'assets/icons/Phone.png'},
+//     {'name': 'Computer', 'IconPath': 'assets/icons/Computer.png'},
+//     {'name': 'Health', 'IconPath': 'assets/icons/Health.png'},
+//     {'name': 'Books', 'IconPath': 'assets/icons/Books.png'},
+//     {'name': 'Phone', 'IconPath': 'assets/icons/Phone.png'},
+//     {'name': 'Phone', 'IconPath': 'assets/icons/Phone.png'},
+//     {'name': 'Phone', 'IconPath': 'assets/icons/Phone.png'}
+//   ];
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       backgroundColor: Colors.black,
+//       body: SingleChildScrollView(
+//         child: Column(
+//           children: [
+//             const SizedBox(
+//               height: 250,
+//             ),
+//             const Text(
+//               'Categories',
+//               style: TextStyle(
+//                 fontSize: 40,
+//                 fontWeight: FontWeight.w700,
+//                 color: Colors.amber,
+//               ),
+//               textAlign: TextAlign.center,
+//             ),
+//             const SizedBox(height: 40),
+//             SizedBox(
+//               height: 150,
+//               child: ListView.builder(
+//                   scrollDirection: Axis.horizontal,
+//                   itemCount: categories.length,
+//                   itemBuilder: (context, index) {
+//                     return Row(
+//                       children: [
+//                         Container(
+//                           padding: const EdgeInsets.all(10),
+//                           margin: const EdgeInsets.only(left: 20),
+//                           decoration: BoxDecoration(
+//                             color: Colors.red,
+//                             borderRadius: BorderRadius.circular(20),
+//                           ),
+//                           child: Image.asset(
+//                             categories[index]['IconPath'],
+//                             height: 50,
+//                           ),
+//                         ),
+//                         const SizedBox(height: 10),
+//                         Text(categories[index]['name'],
+//                         style: const TextStyle(
+//                           color: Colors.white,
+//                         ),
+//                           textAlign: TextAlign.center
+//                         )
+//                       ],
+//                     );
+//                   }),
+//             )
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
 
 class _HotSalesWidget extends StatelessWidget {
   const _HotSalesWidget({
@@ -182,3 +220,6 @@ class _SearchWidget extends StatelessWidget {
 //     );
 //   }
 // }
+
+
+
---
+++ lib/application/ui/screens/home_screen/list_category.dart
@@ -0,0 +1,10 @@
+List listCategory = [
+  {'name': 'Phone', 'IconPath': 'assets/icons/Phone.svg'},
+  {'name': 'Computer', 'IconPath': 'assets/icons/Computer.svg'},
+  {'name': 'Health', 'IconPath': 'assets/icons/heart.svg'},
+  {'name': 'Books', 'IconPath': 'assets/icons/books.svg'},
+  {'name': 'Phone1', 'IconPath': 'assets/icons/phone1.svg'},
+  {'name': 'Phone', 'IconPath': 'assets/icons/Phone.svg'},
+  {'name': 'Phone', 'IconPath': 'assets/icons/Phone.svg'}
+
+];
