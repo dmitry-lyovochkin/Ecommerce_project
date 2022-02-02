59819c2f8214fdb177b4fad4e60ef95d26eb1267
Dmitry <dmitry.lyovochkin@gmail.com>
2022-02-02 17:38:30 +0300

    переписать чище BottomNavBar

--- lib/application/ui/screens/home_screen/Button_nav_bar/main_tabs_screen.dart
+++ lib/application/ui/screens/home_screen/Button_nav_bar/main_tabs_screen.dart
@@ -1,105 +1,110 @@
 import 'package:ecommerce_project/application/ui/screens/home_screen/category_widget.dart';
-import 'package:ecommerce_project/application/ui/screens/home_screen/Button_nav_bar/main_tabs_view_model.dart';
 import 'package:ecommerce_project/application/ui/themes/app_theme.dart';
 import 'package:ecommerce_project/application/ui/themes/custom_icons.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
-class MainTabsScreen extends StatelessWidget {
-  const MainTabsScreen({Key? key}) : super(key: key);
+// class MainTabsScreen extends StatelessWidget {
+//   const MainTabsScreen({Key? key}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return const Scaffold(
+//       body: _BodyWidget(), 
+//       bottomNavigationBar: _NavBarWidget()
+//     );
+//   }
+// }
+
+// class _BodyWidget extends StatelessWidget {
+//   const _BodyWidget({
+//     Key? key,
+//   }) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     final currentIndex =
+//         context.select((MainTabsViewModel vm) => vm.currentTabIndex);
+//     return IndexedStack(
+//       index: currentIndex,
+//       children: const [
+//         CategoryWidget(),
+//         Center(child: Text('1')),
+//         Center(child: Text('2')),
+//         Center(child: Text('3')),
+//         Center(child: Text('4')),
+//       ],
+//     );
+//   }
+// }
+
+// class _NavBarWidget extends StatelessWidget {
+//   const _NavBarWidget({
+//     Key? key,
+//   }) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     final model = context.read<MainTabsViewModel>();
+//     final currentIndex =
+//         context.select((MainTabsViewModel vm) => vm.currentTabIndex);
+//     return ClipRRect(
+//       borderRadius: BorderRadius.circular(25),
+//       child: SizedBox(
+//         height: 69,
+//         child: BottomNavigationBar(
+//           currentIndex: currentIndex,
+//           onTap: model.setCurrentTabIndex,
+//           type: BottomNavigationBarType.fixed,
+//           showSelectedLabels: false,
+//           showUnselectedLabels: false,
+//           elevation: 0,
+//           // backgroundColor: Colors.black,
+//           backgroundColor: AppColors.buttonBarColor,
+//           items:  [
+//             BottomNavigationBarItem(
+//               icon: 
+//                 Row(
+//                   mainAxisAlignment: MainAxisAlignment.end,
+//                   children: const [
+//                     Text(
+//                       '●   Explorer',
+//                       style: TextStyle(
+//                         color: Colors.white, 
+//                         fontSize: 15
+//                       ),
+//                     ),
+//                   ],
+//                 ),
+//               label: "",
+//             ),
+//             const BottomNavigationBarItem(
+//               icon: Icon(CustomIcons.vector, size: 19),
+//               label: "",
+//             ),
+//             const BottomNavigationBarItem(
+//               icon: Icon(CustomIcons.vector__1_, size: 18),
+//               label: "",
+//             ),
+//             const BottomNavigationBarItem(
+//               icon: Icon(CustomIcons.profile, size: 18),
+//               label: "",
+//             ),
+//           ]
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+
+class BottomNavBarWidget extends StatefulWidget {
+  const BottomNavBarWidget({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return const Scaffold(
-      body: _BodyWidget(), 
-      bottomNavigationBar: _NavBarWidget()
-    );
-  }
+  State<BottomNavBarWidget> createState() => _BottomNavBarWidgetState();
 }
 
-class _BodyWidget extends StatelessWidget {
-  const _BodyWidget({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    final currentIndex =
-        context.select((MainTabsViewModel vm) => vm.currentTabIndex);
-    return IndexedStack(
-      index: currentIndex,
-      children: const [
-        CategoryWidget(),
-        Center(child: Text('1')),
-        Center(child: Text('2')),
-        Center(child: Text('3')),
-        Center(child: Text('4')),
-      ],
-    );
-  }
-}
-
-class _NavBarWidget extends StatelessWidget {
-  const _NavBarWidget({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    final model = context.read<MainTabsViewModel>();
-    final currentIndex =
-        context.select((MainTabsViewModel vm) => vm.currentTabIndex);
-    return ClipRRect(
-      borderRadius: BorderRadius.circular(25),
-      child: SizedBox(
-        height: 69,
-        child: BottomNavigationBar(
-          currentIndex: currentIndex,
-          onTap: model.setCurrentTabIndex,
-          type: BottomNavigationBarType.fixed,
-          elevation: 5,
-          // backgroundColor: Colors.black,
-          backgroundColor: AppColors.buttonBarColor,
-          items: const [
-            BottomNavigationBarItem(
-              icon: 
-                Text(
-                  '●   Explorer',
-                  style: TextStyle(
-                    color: Colors.white, 
-                    fontSize: 15
-                  ),
-                ),
-              label: "",
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(CustomIcons.vector, size: 19),
-              label: "",
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(CustomIcons.vector__1_, size: 18),
-              label: "",
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(CustomIcons.profile, size: 18),
-              label: "",
-            ),
-          ]
-        ),
-      ),
-    );
-  }
-}
-
-
-class MyStatefulWidget extends StatefulWidget {
-  const MyStatefulWidget({Key? key}) : super(key: key);
-
-  @override
-  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
-}
-
-class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+class _BottomNavBarWidgetState extends State<BottomNavBarWidget> {
   int _selectedIndex = 0;
   static const List _widgetOptions = [
     CategoryWidget(),
@@ -122,42 +127,50 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
 
   @override
   Widget build(BuildContext context) {
-    // return ClipRRect(
-    //   borderRadius: BorderRadius.circular(25),
-      // child: Scaffold(
-      //   height: 69,
       return Scaffold(
-        // height: 69,
-        // width: double.maxFinite,
-        // color: Colors.amber,
       body:  _widgetOptions.elementAt(_selectedIndex),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const [ 
-          BottomNavigationBarItem(
-                // backgroundColor: Colors.amber,
-                icon: Text(
-                  '●   Explorer',
-                  /* Explorer */
-                  style: TextStyle(color: Colors.black, fontSize: 15),
+      bottomNavigationBar: ClipRRect(
+        borderRadius: BorderRadius.circular(25),
+        child: SizedBox(
+          height: 69,
+          child: BottomNavigationBar(
+            type: BottomNavigationBarType.fixed,
+            showSelectedLabels: false,
+            showUnselectedLabels: false,
+            backgroundColor: AppColors.buttonBarColor,
+            unselectedItemColor: Colors.white,
+            onTap: _onItemTapped,
+            items: [   
+              BottomNavigationBarItem(
+                icon: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: const [
+                    Text(
+                      '●   Explorer',
+                      style: TextStyle(
+                        color: Colors.white, 
+                        fontSize: 15
+                      ),
+                    ),
+                  ],
                 ),
                 label: "",
               ),
-              BottomNavigationBarItem(
+              const BottomNavigationBarItem(
                 icon: Icon(CustomIcons.vector, size: 19),
                 label: "",
               ),
-              BottomNavigationBarItem(
+              const BottomNavigationBarItem(
                 icon: Icon(CustomIcons.vector__1_, size: 18),
                 label: "",
               ),
-              BottomNavigationBarItem(
+              const BottomNavigationBarItem(
                 icon: Icon(CustomIcons.profile, size: 18),
                 label: "",
               ),
-        ],
-      currentIndex: _selectedIndex,
-      onTap: _onItemTapped,
-      type: BottomNavigationBarType.fixed,
+            ],
+          ),
+        ),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,23 @@
-import 'package:ecommerce_project/application/ui/my_app/my_app.dart';
+import 'package:ecommerce_project/application/ui/screens/home_screen/Button_nav_bar/main_tabs_screen.dart';
+import 'package:ecommerce_project/application/ui/themes/app_theme.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(
+    const MyApp()
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: AppTheme.light,
+      debugShowCheckedModeBanner: false,
+      home: const BottomNavBarWidget(),
+    );
+  }
 }
